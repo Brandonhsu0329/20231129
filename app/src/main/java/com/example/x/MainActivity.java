3882eb9bef1e1f3927be4e7e32eac1b097e8b498
Brandonhsu0329 <brandonhsu329@gmail.com>
2023-11-29 14:31:27 +0800

    20231129

--- app/src/main/java/com/example/x/MainActivity.java
+++ app/src/main/java/com/example/x/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.x; //注意
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
+import android.provider.DocumentsContract;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileDescriptor;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import android.database.Cursor;
 import android.net.Uri;
@@ -28,20 +43,73 @@ public class MainActivity extends AppCompatActivity {
     // private MFCCCalculator mfccCalculator;
     private TextView mfccTextView;
 
+//    private String convertStreamToString(InputStream is) {
+//        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+//        StringBuilder stringBuilder = new StringBuilder();
+//        String line;
+//
+//        try {
+//            while ((line = reader.readLine()) != null) {
+//                stringBuilder.append(line).append('\n');
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        } finally {
+//            try {
+//                is.close();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//
+//        return stringBuilder.toString();
+//    }
+
     private String getPathFromUri(Uri uri) {
         String[] projection = {MediaStore.Audio.Media.DATA};
         Cursor cursor = getContentResolver().query(uri, projection, null, null, null);
 
-        if (cursor != null) {
+//        try {
+//            InputStream inputStream = getContentResolver().openInputStream(uri);
+//
+//            if (inputStream != null) {
+//                // 將 InputStream 轉換為字串
+//                String content = convertStreamToString(inputStream);
+//
+//                // 在這裡處理完整的內部字串
+//                Log.d("Tag", "Content: " + content);
+//
+//                // 關閉 InputStream
+//                inputStream.close();
+//            }
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//            // 在這裡處理檔案不存在的情況
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//            // 在這裡處理其他 IO 異常
+//        }
+
+
+        if (cursor != null && cursor.moveToFirst()) {
+//            String filePath = getPathFromUri(uri);
+
             int column_index = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DATA);
-            cursor.moveToFirst();
+            Log.d("Tag", "uri: "+ uri);
+//            cursor.moveToFirst();
             String filePath = cursor.getString(column_index);
+            Log.d("Tag", "cursor: "+ cursor);
             cursor.close();
+            Log.d("Tag", "filePath: "+ filePath);
             return filePath;
+
+        }
+        return null;
+
         }
 
-        return null;
-    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,42 +125,85 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 // 在按鈕點擊事件中處理 MFCC 計算邏輯
+                Log.d("Tag", "i am here_1");
                 showFileChooser();
             }
         });
     }
 
+    private final ActivityResultLauncher<Intent> someActivityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == Activity.RESULT_OK) {
+                    Intent data = result.getData();
+//                    Log.d("Tag", String.format("data: %s", data));
+                    doSomeOperations(data);
+                }
+            }
+    );
+
     private void showFileChooser() {
         Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
         intent.addCategory(Intent.CATEGORY_OPENABLE);
         intent.setType("audio/*"); // 接受所有音訊格式
+        Log.d("Tag", "i am here_2");
 
-        startActivityForResult(intent, READ_REQUEST_CODE); //注意
+        someActivityResultLauncher.launch(intent);
+        Log.d("Tag", "i am here_3");
+    }
+    private void doSomeOperations() {
+        // 在這裡執行你希望在 Activity 結束後執行的操作
+        Log.d("Tag", "doSomeOperations executed");
+        // 其他操作...
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+    private void doSomeOperations(Intent data) {
 
-        if (requestCode == READ_REQUEST_CODE) {
-            if (resultCode == RESULT_OK && data != null) {
-                // 獲取選擇的檔案路徑
-                String filePath = getPathFromUri(data.getData());
+        if (data != null) {
+            // 獲取選擇的檔案路徑
+//            Log.d("Tag", "data: "+ data);
+            String filePath = getPathFromUri(data.getData());
+//            Log.d("Tag", "fillPath: "+ getPathFromUri(data.getData()));
 
-                if (filePath != null) {
-                    // 計算 MFCC 並更新 TextView
-                    float[] mfccValues = calculateMFCC(filePath);
-                    updateMFCCTextView(mfccValues);
+            if (filePath != null) {
+                // 計算 MFCC 並更新 TextView
 
-                    // 在這裡添加上傳成功的處理代碼
-                    showUploadSuccessMessage();
-                }
-            } else {
-                // 處理取消選擇或其他情況
-                Toast.makeText(this, "選擇取消或發生錯誤", Toast.LENGTH_SHORT).show();
+
+                float[] mfccValues = calculateMFCC(filePath);
+                updateMFCCTextView(mfccValues);
+
+                // 在這裡添加上傳成功的處理代碼
+                showUploadSuccessMessage();
             }
+        } else {
+            // 處理取消選擇或其他情況
+            Toast.makeText(this, "選擇取消或發生錯誤", Toast.LENGTH_SHORT).show();
         }
     }
+
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//
+//        if (requestCode == READ_REQUEST_CODE) {
+//            if (resultCode == RESULT_OK && data != null) {
+//                // 獲取選擇的檔案路徑
+//                String filePath = getPathFromUri(data.getData());
+//
+//                if (filePath != null) {
+//                    // 計算 MFCC 並更新 TextView
+//                    float[] mfccValues = calculateMFCC(filePath);
+//                    updateMFCCTextView(mfccValues);
+//
+//                    // 在這裡添加上傳成功的處理代碼
+//                    showUploadSuccessMessage();
+//                }
+//            } else {
+//                // 處理取消選擇或其他情況
+//                Toast.makeText(this, "選擇取消或發生錯誤", Toast.LENGTH_SHORT).show();
+//            }
+//        }
+//    }
 
     private float[] calculateMFCC(String filePath) {
         try {
@@ -107,10 +218,11 @@ public class MainActivity extends AppCompatActivity {
 //            dispatcher.addAudioProcessor(new AndroidAudioPlayer(TarsosDSPAudioFormat.toTarsosDSPFormat(TarsosDSPAudioFormat.Encoding.PCM_SIGNED, sampleRate, 16, 1, 2, sampleRate), bufferSize, bufferOverlap));
 
             dispatcher.run();
-
+            Log.d("Tag", "mfcc: "+ Arrays.toString(mfcc.getMFCC()));
             return mfcc.getMFCC();
         } catch (Exception e) {
             e.printStackTrace();
+            Log.d("Tag", "error123");
         }
 
         return new float[0];
